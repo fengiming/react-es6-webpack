b8ceefc6822190c29b50c51e34afe7f2aedffea0
feng.zou <fengiming@users.noreply.github.com>
2016-10-11 11:14:17 +0800

    Add files via upload

--- es6/gulpfile.js
+++ es6/gulpfile.js
@@ -2,6 +2,7 @@
  * Created by Administrator on 2016/8/11.
  */
 var webpack = require('webpack');
+var fs = require('fs');
 var WebpackDevServer = require('webpack-dev-server');
 var gulp = require('gulp');
 var gulpUtil = require('gulp-util');
@@ -10,6 +11,7 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var config = require('./webpack.config.js');
 var build = './build';
+var bundleName = 'app' ;
 // 拷贝文件
 gulp.task('html', function () {
     gulp.src('./index.html')
@@ -25,17 +27,34 @@ gulp.task('css', function (callback) {
 
 // webpack打包
 gulp.task('webpack', function (callback) {
-    webpack(config, function (err, stats) {
-        if (err) throw new gulpUtil.PluginError("webpack", err);
-        callback();
-    });
+    var bundler = webpack(config);
+    /* 替换对应的文件路径 */
+    bundler.run(function (err, stats) {
+        var assets = stats.toJson().assets;
+        var name;
+        console.info(assets);
+        for (var i = 0; assets[i]; i++) {
+            if (assets[i].name.indexOf(bundleName) === 0 && assets[i].name.indexOf('.map') < 0) {
+                name = assets[i].name;
+                break
+            }
+        }
+        console.info('重新打包后的js文件名：' + name);
+        fs.stat(config.buildTemplatePath, function (err, stats) {
+            if (err) {
+                return;
+            }
+            // 修改对应的HTML模板文件
+            writeTemplate(name);
+        })
+    })
 });
 
 gulp.task('webpackDevServer', function () {
     // Start a webpack-dev-server
     var compiler = webpack(config);
     new WebpackDevServer(compiler, {
-        contentBase:'build',
+        contentBase: 'build',
         hot: true
     }).listen(8080, "localhost", function (err) {
             if (err) throw new gulpUtil.PluginError("webpack-dev-server", err);
@@ -43,3 +62,25 @@ gulp.task('webpackDevServer', function () {
             gulpUtil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
         });
 });
+
+// 替换页面中的js引用路径
+function writeTemplate(name, oldName) {
+    var fstring, oldName = oldName || bundleName, targetStr, matchStr;
+    var rscript = '<script\\s+.*(?:src=[\'\"][-\\w\\/]*(' + oldName + '[-\\w]*\\.js)[\'\"]).*>';
+    fs.readFile(config.buildTemplatePath, function (err, data) {
+        if (err) {
+            return;
+        }
+        /* 若文件内容不为空 */
+        if ((fstring = data.toString()).length > 0) {
+            if (targetStr = (matchStr = fstring.match(new RegExp(rscript)) || [])[1]) {
+                fstring = fstring.replace(targetStr, name);
+                fs.writeFile(config.buildTemplatePath, fstring, function (err) {
+                    if (err) {
+                        console.info(err);
+                    }
+                });
+            }
+        }
+    });
+}
--- es6/webpack.config.js
+++ es6/webpack.config.js
@@ -3,17 +3,28 @@
  */
 var path = require('path') ;
 var webpack = require('webpack');
+var RenamePlugin = require('./plugin/rename.plugin.js');
 module.exports = {
     devtool:'source-map',// 该参数对开发很有帮助
-    entry: [
-        'webpack-dev-server/client?http://localhost:8080/',
-        'webpack/hot/dev-server',
-        './app/app.js'
-    ],
+    /*externals: {
+        'react': 'React',
+        'react-dom':'ReactDOM',
+        'react-router':'ReactRouter'
+    },*/
+    buildTemplatePath:path.resolve(__dirname,'build/index.html'),
+    entry: {
+        vendor: ["jquery", "react",'react-dom','react-router'],
+        app:[
+            //'webpack-dev-server/client?http://localhost:8080/',
+            //'webpack/hot/dev-server',
+            './app/app.js'
+        ]
+    },
     output: {
-        path: path.resolve(__dirname, "build"),
+        path: path.resolve(__dirname, "build"),// 要求是绝对地址
         publicPath: '/assets/',
-        filename: 'bundle.js'
+        // 以文件内容MD5值作为文件名后缀，可以避免缓存导致的问题
+        filename: 'app-[hash:6].js'
     },
     module: {
         loaders: [
@@ -41,6 +52,15 @@ module.exports = {
                 comments: false
             }
         }),
-        new webpack.HotModuleReplacementPlugin()
+        //new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+            filename: "vendor.js",
+            // (Give the chunk a different name)
+            minChunks: Infinity
+            // (with more entries, this ensures that no other module
+            //  goes into the vendor chunk)
+        }),
+       // new RenamePlugin()
     ]
 };
